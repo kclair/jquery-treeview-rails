5e100d1f791875bbf15965dabc708dadf926cbf6
k clair <kclair@pickledradish.com>
2012-01-02 23:54:10 +0000

    add inherited engine class

--- lib/jquery_treeview/rails.rb
+++ lib/jquery_treeview/rails.rb
@@ -1,5 +1,6 @@
 class JqueryTreeview
   module Rails
     require 'jquery_treeview/rails/version.rb'
+    require 'jquery_treeview/rails/engine.rb'
   end
 end
---
+++ lib/jquery_treeview/rails/engine.rb
@@ -0,0 +1,7 @@
+# Configure Rails 3.1 
+class Jquery
+  module Rails
+    class Engine < ::Rails::Engine
+    end
+  end
+end

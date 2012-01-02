43c07f0c24d81819fb88791bd0545b7349f77dde
k clair <kclair@pickledradish.com>
2012-01-02 23:11:17 +0000

    uncomplicate the directory structure

--- lib/jquery_treeview/rails.rb
+++ lib/jquery_treeview/rails.rb
@@ -1,5 +1,6 @@
-module JqueryTreeview
+class JqueryTreeview
   module Rails
-    require 'jquery/rails/version'
+    VERSION = "0.1.1"
+    JQUERY_TREEVIEW_VERSION = "1.4.1"
   end
 end
--- lib/jquery_treeview/rails/version.rb
+++
@@ -1,6 +0,0 @@
-module JqueryTreeview
-  module Rails
-    VERSION = "0.1.1"
-    JQUERY_TREEVIEW_VERSION = "1.4.1"
-  end
-end
